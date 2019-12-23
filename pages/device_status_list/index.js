e3bbadf1ab9109ce98d99cc349ea57288304dce5
yc <3095220780@qq.com>
2019-12-23 11:07:46 +0800

    设备状态监控

--- pages/device_status_list/index.js
+++ pages/device_status_list/index.js
@@ -1,91 +1,41 @@
 Page({
   data: {
-    list:[
-      {
-        listName:'列表1',
-        item:[
-          {
-            itemName:'子列表1-1',
-            content:'1-1中的内容',
-            time: '2015-05-06'
-          }, 
-          {
-              itemName: '子列表1-2',
-              content: '1-2中的内容',
-              time: '2015-04-13'
-          }, 
-          {
-              itemName: '子列表1-3',
-              content: '1-3中的内容',
-              time: '2015-12-06'
-          }
-        ]
-      }, 
-      {
-        listName: '列表2',
-        item: [
-          {
-            itemName: '子列表2-1',
-            content: '2-1中的内容',
-            time: '2017-05-06'
-          }, 
-          {
-            itemName: '子列表2-2',
-            content: '2-2中的内容',
-            time: '2015-08-06'
-          }, 
-          {
-            itemName: '子列表2-3',
-            content: '2-3中的内容',
-            time: '2015-11-06'
-          }
-        ]
-      }, 
-      {
-        listName: '列表3',
-        item: [
-          {
-            itemName: '子列表3-1',
-            content: '3-1中的内容',
-            time: '2015-05-15'
-          }, 
-          {
-            itemName: '子列表3-2',
-            content: '3-2中的内容',
-            time: '2015-05-24'
-          }, 
-          {
-            itemName: '子列表3-3',
-            content: '3-3中的内容',
-            time: '2015-05-30'
-          }
-        ]
-      }
-    ]
+    list: [{
+      id: 'view',
+      name: '河北保定容城',
+      open: false,
+      pages: ['view', 'scroll-view', 'swiper', 'movable-view', 'cover-view']
+    }, {
+      id: 'content',
+      name: '星沙水厂',
+      open: false,
+      pages: ['text', 'icon', 'progress', 'rich-text']
+    }, {
+      id: 'form',
+      name: '瑞红盐业',
+      open: false,
+      pages: ['button', 'checkbox', 'form', 'input', 'label', 'picker', 'picker-view', 'radio', 'slider', 'switch', 'textarea']
+    }]
   },
-  //点击最外层列表展开收起
-  listTap(e){
-    let Index = e.currentTarget.dataset.parentindex,//获取点击的下标值
-        list=this.data.list;
-    list[Index].show = !list[Index].show || false;//变换其打开、关闭的状态
-    if (list[Index].show){//如果点击后是展开状态，则让其他已经展开的列表变为收起状态
-      this.packUp(list,Index);
+  kindToggle(e) {
+    const id = e.currentTarget.id
+    const list = this.data.list
+    for (let i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id === id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
     }
 
+    /**
+     * key和value名称一样时，可以省略
+     *
+     * list:list=>list
+     */
     this.setData({
       list
-    });
-  },
-  //让所有的展开项，都变为收起
-  packUp(data,index){
-    for (let i = 0, len = data.length; i < len; i++) {//其他最外层列表变为关闭状态
-      if(index!=i){
-        data[i].show = false;
-        for (let j=0;j<data[i].item.length;j++){//其他所有内层也为关闭状态
-            data[i].item[j].show=false;
-        }
-      }
-    }
+    })
   },
   chooseDevice(e){
     // console.log(e.currentTarget.dataset.device)
